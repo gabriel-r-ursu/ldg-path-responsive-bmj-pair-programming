45c8799f8b1895d08b7290ed1734c6fcfb4f5dcd
Gabriel Ursu <gabrielursu@Gabriels-MacBook-Pro.local>
2021-11-29 12:37:24 +0200

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components'
+
+const TestWrapper = styled.section`
+/*Test values to check if styled-components work. */
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <TestWrapper>
+      <p>This does not error out.</p>
+      <p>Styled-components work in the app as well.</p>
+    </TestWrapper>
   );
 }
 
