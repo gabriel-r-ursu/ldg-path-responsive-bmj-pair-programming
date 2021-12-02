3989b83e6f4e1b53a737ab122f279a9957e585db
Florin Dobinciuc <florindobinciuc@Florins-MBP.Dlink>
2021-12-02 10:05:32 +0200

    styled navbar

--- src/styles/NavbarStyles/Navbar.styled.tsx
+++ src/styles/NavbarStyles/Navbar.styled.tsx
@@ -1,8 +1,11 @@
 import styled from "styled-components";
 
 const StyledNavbar = styled.nav`
-height: 3rem;
-background-color: #2a6eba;
+  height: 3rem;
+  background-color: #2a6eba;
+  display: flex;
+  justify-content: space-around;
+  padding: 0 1rem;
 `;
 
 export default StyledNavbar;
